d5fe1b75603e6632290fabe23687363a639e0786
kevin <yundan_wy@126.com>
2022-04-02 09:26:15 +0800

    SearcherApplication索引检索

---
+++ lucene-sample/src/test/java/com/kevin/lucene/sample/CRUDLuceneAPITest.java
@@ -0,0 +1,45 @@
+package com.kevin.lucene.sample;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+/**
+ * lucene API 增删改查测试
+ *
+ * @author kevin
+ */
+public class CRUDLuceneAPITest {
+
+
+
+
+  @Test
+  @DisplayName("添加文档")
+  public void addDocumentTest(){
+
+
+
+
+  }
+
+
+  @Test
+  @DisplayName("删除文档")
+  public void deleteDocumentTest(){
+
+  }
+
+
+
+  @Test
+  public void searchTest(){
+
+
+  }
+
+
+
+
+
+
+}
