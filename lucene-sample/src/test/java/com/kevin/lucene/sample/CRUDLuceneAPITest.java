5b386a830475874ddebdbea39a27ff2b905ec940
wangyong <wb_wangyong02@meituan.com>
2022-04-02 17:20:21 +0800

    添加javaDoc注释

--- lucene-sample/src/test/java/com/kevin/lucene/sample/CRUDLuceneAPITest.java
+++ lucene-sample/src/test/java/com/kevin/lucene/sample/CRUDLuceneAPITest.java
@@ -59,6 +59,28 @@ public class CRUDLuceneAPITest {
     }
 
 
+    /**
+     * IndexWriter提供删除文档API:
+     * 1. IndexWriter.deleteDocuments(Term term) 负责删除包含项的所有文档。
+     * 2. IndexWriter.deleteDocuments(Term[] terms) 负责删除包含项数组任一元素的所有文档。
+     * 3. IndexWriter.deleteDocuments(Query query) 负责删除匹配查询语句的所有文档。
+     * 4. IndexWriter.deleteDocuments(Query[] queries) 负责删除匹配查询语句数组任一元素的所有文档。
+     * 5. IndexWriter.deleteAll() 负责删除索引中全部文档。
+     * ================================================
+     * Tip:
+     *
+     * 如果需要通过Term删除单个文档(IndexWriter.deleteDocuments(Term term))，需要确认文档在索引时索引了指定Field字段，
+     * 还需要确认该Field域字段值都是唯一的，才可以将该文档单独定位出来并删除。
+     * 概念类似于数据库中的主键，需要注意该域需要设置为“不分析”的域，保证分析器不会将它分解成语汇单元。
+     *
+     * IndexWriter.deleteDocuments(Term term)
+     * 注意：调用该方法时一定要谨慎，如果不小心创建了错误的Term对象（被分词的域，可能关联很多文档），那么Lucene
+     * 将删除索引中与该Term命中的全部文档。
+     * 删除操作并不会立即执行，而是会存放在缓冲区内，与加入文档类似，最后Lucene会通过周期性刷新文档目录来执行该操作。
+     * 也可以通过writer.commit() 或者 writer.close() 来立即生效。即使删除操作已经完成，物理磁盘上的文件也不会立即
+     * 删除，Lucene只是将被删除的文档标记为“删除”，待索引段合并时会进行真正的物理删除。
+     *
+     */
     @Test
     @DisplayName("删除文档")
     public void deleteDocumentTest() throws IOException {
@@ -93,7 +115,18 @@ public class CRUDLuceneAPITest {
         indexWriter.close();
     }
 
+    /**
+     * 更新已索引的文档这个需求很常见，比如你的搜索系统依赖的源数据进行的更新，那么相对应的索引就必须进行更新，否则搜索系统的搜索准确度会降低。
+     * 某些情况下，仅仅是文档的某个域需要更新，如产品名称发生了变化，但是正文未变化，非常遗憾，尽管该需求很普遍，但是Lucene做不到。
+     * Lucene只能删除整个过期文档，然后再向索引中添加新文档。这要求新文档必须包含旧文档的所有域，包括内容未发生改变的域。
+     * IndexWriter 提供了1个API来更新索引中的文档:IndexWriter.updateDocument(Term term, Document newDoc)
+     * 第一步 删除term匹配的文档，第二步 使用writer再添加文档。
+     * 例：IndexWriter.updateDocument(new Term("id", documentId), newDocument)
+     * tip：由于updateDocument方法在后台会调用deleteDocuments方法，一定要确定Term标识的唯一性。
+     *
+     */
     @Test
+    @DisplayName("更新文档")
     public void updateDocumentTest() throws IOException {
         addDocument();
 
