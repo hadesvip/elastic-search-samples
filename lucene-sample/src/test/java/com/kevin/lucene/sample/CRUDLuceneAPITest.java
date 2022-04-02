343cfd904e51c5b339946f422e91cd33fefc6fd6
wangyong <wb_wangyong02@meituan.com>
2022-04-02 16:15:25 +0800

    增加添加文档和检索文档测试

--- lucene-sample/src/test/java/com/kevin/lucene/sample/CRUDLuceneAPITest.java
+++ lucene-sample/src/test/java/com/kevin/lucene/sample/CRUDLuceneAPITest.java
@@ -1,7 +1,18 @@
 package com.kevin.lucene.sample;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.apache.lucene.analysis.core.WhitespaceAnalyzer;
+import org.apache.lucene.document.*;
+import org.apache.lucene.index.*;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.TermQuery;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.RAMDirectory;
+import org.junit.jupiter.api.*;
+
+import java.io.IOException;
+import java.util.logging.Logger;
+import java.util.stream.Stream;
 
 /**
  * lucene API 增删改查测试
@@ -10,36 +21,61 @@ import org.junit.jupiter.api.Test;
  */
 public class CRUDLuceneAPITest {
 
+    private Directory directory;
+    private IndexWriter indexWriter;
+
+    /*========写入测试数据=========*/
+    private final String[] idArray = {"1", "2"};
+    private final String[] unIndexedArray = {"Netherlands", "Italy"};
+    private final String[] unStoredArray = {"Amsterdam has lots of briges", "Venice has lots of canals"};
+    private final String[] textArray = {"Amsterdam", "Venice"};
 
 
+    @BeforeEach
+    public void init() throws IOException {
+        directory = new RAMDirectory();
+        //按照空格分词
+        IndexWriterConfig indexWriterConfig = new IndexWriterConfig(new WhitespaceAnalyzer());
+        indexWriter = new IndexWriter(directory, indexWriterConfig);
+    }
 
-  @Test
-  @DisplayName("添加文档")
-  public void addDocumentTest(){
+    @Test
+    @DisplayName("删除文档")
+    public void deleteDocumentTest() {
 
-
-
-
-  }
-
-
-  @Test
-  @DisplayName("删除文档")
-  public void deleteDocumentTest(){
-
-  }
+    }
 
 
+    @Test
+    @DisplayName("检索测试")
+    public void searchTest() throws IOException {
+        addDocument();
+        destroy();
+        IndexReader indexWriter = DirectoryReader.open(directory);
+        IndexSearcher indexSearcher = new IndexSearcher(indexWriter);
+        Term term = new Term("city", "Amsterdam");
+        Query termQuery = new TermQuery(term);
+        int hit = indexSearcher.search(termQuery, 1).totalHits;
+        Logger.getGlobal().info(String.format("searcher hits:%s", hit));
+    }
 
-  @Test
-  public void searchTest(){
+    void destroy() throws IOException {
+        String content = String.format("索引中管理文档数量:%s,程序写入的文档数量:%s",
+                indexWriter.numDocs(), idArray.length);
+        Logger.getGlobal().info(content);
+        //关闭writer或者调用writer.commit()都会将缓冲区内的索引数据落盘
+        indexWriter.close();
+    }
 
-
-  }
-
-
-
-
-
+    void addDocument() throws IOException {
+        for (int i = 0; i < idArray.length; i++) {
+            Document document = new Document();
+            document.add(new StringField("id", idArray[i], Field.Store.YES));
+            document.add(new StoredField("country", unIndexedArray[i]));
+            document.add(new TextField("contents", unStoredArray[i], Field.Store.NO));
+            document.add(new TextField("city", textArray[i], Field.Store.YES));
+            indexWriter.addDocument(document);
+        }
+    }
 
 }
